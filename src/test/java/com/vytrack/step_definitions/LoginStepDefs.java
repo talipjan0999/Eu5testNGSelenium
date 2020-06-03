cc06a9c6b185c820d07d87f54c5078a757ac3a47
CybertekSchool <github@cybertekschool.com>
2020-06-03 15:27:43 -0400

    day3 add files 2

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -1,38 +1,60 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
 
 public class LoginStepDefs {
 
 
     @Given("the user is on the login page")
-    public void the_user_is_on_the_login_page() throws InterruptedException {
-        Thread.sleep(5000);
-        System.out.println("I open browser and navigated to vytrack login page");
+    public void the_user_is_on_the_login_page() {
+        //WebDriver driver = Driver.get();
+        //driver.get(url);
+        //instead of driver object we will use Driver.get() directly
+        String url = ConfigurationReader.get("url");
+        Driver.get().get(url);
     }
 
     @When("the user enter the driver information")
-    public void the_user_enter_the_driver_information() throws InterruptedException {
-        Thread.sleep(3000);
-        System.out.println("I put user1 username and UserUser123 password and click login button");
+    public void the_user_enter_the_driver_information(){
+        String username = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
+
     }
 
     @Then("the user should be able to login")
-    public void the_user_should_be_able_to_login() throws InterruptedException {
-        Thread.sleep(2000);
-        System.out.println("I verify that title changed to Dashboard");
+    public void the_user_should_be_able_to_login() {
+        BrowserUtils.waitFor(3);
+
+        String actualTitle = Driver.get().getTitle();
+        Assert.assertEquals("Verify title","Dashboard",actualTitle);
+
+
     }
 
     @When("the user enter sales manager information")
     public void the_user_enter_sales_manager_information() {
-        System.out.println("I put salesmanager85 and UserUser123 and click enter");
+        String username = ConfigurationReader.get("sales_manager_username");
+        String password = ConfigurationReader.get("sales_manager_password");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
     }
     @When("the user enter store manager information")
     public void the_user_enter_store_manager_information() {
-        System.out.println("I put storemanager101 and UserUser123 and click enter");
-
+        String username = ConfigurationReader.get("store_manager_username");
+        String password = ConfigurationReader.get("store_manager_password");
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username,password);
     }
 
 
--- src/test/resources/features/Login.feature
+++ src/test/resources/features/Login.feature
@@ -1,11 +1,11 @@
 @login
 Feature: Users should be able to login
-  @driver @smoke @VYT-123
+  @driver @smoke @VYT-123 @wip
   Scenario: Login as a driver
     Given the user is on the login page
     When the user enter the driver information
     Then the user should be able to login
-  @sales_manager @VYT-123 @wip
+  @sales_manager @VYT-123
   Scenario: Login as a sales manager
     Given the user is on the login page
     When the user enter sales manager information
