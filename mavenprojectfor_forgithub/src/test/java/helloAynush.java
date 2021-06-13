15c798e2c4711a4fbd4006f85d9ead7ae847e7ae
talipjan0999 <talipjan1208@Gmail.com>
2021-06-13 23:46:58 +0200

    add more

---
+++ mavenprojectfor_forgithub/src/test/java/helloAynush.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class helloAynush {
+    public static void main(String[] args) {
+        System.out.println("myfirst_meeting is in Augost");
+        System.out.println("second maven ");
+    }
+
+
+}
