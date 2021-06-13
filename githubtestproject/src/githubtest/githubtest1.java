92d1b965fc2a466c150e792dbb6282e043eb9f7a
talipjan0999 <talipjan1208@Gmail.com>
2021-06-13 23:30:46 +0200

    add more work

--- githubtestproject/src/githubtest/githubtest1.java
+++ githubtestproject/src/githubtest/githubtest1.java
@@ -4,6 +4,7 @@ public class githubtest1 {
     public static void main(String[] args) {
         System.out.println("it is my first github test");
         System.out.println(" = " + "secodn");
+        System.out.println("threed commit");
 
     }
 }
