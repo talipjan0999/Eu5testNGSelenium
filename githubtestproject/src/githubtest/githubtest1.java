7cdfa0094f950f32f5712a50a81f3796fbaac878
talipjan0999 <talipjan1208@Gmail.com>
2021-06-13 23:27:26 +0200

    added second commit

--- githubtestproject/src/githubtest/githubtest1.java
+++ githubtestproject/src/githubtest/githubtest1.java
@@ -3,6 +3,7 @@ package githubtest;
 public class githubtest1 {
     public static void main(String[] args) {
         System.out.println("it is my first github test");
+        System.out.println(" = " + "secodn");
 
     }
 }
